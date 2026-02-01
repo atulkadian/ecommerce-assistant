dfcbb5fd42a3573f55713087ee3964da9e0e5433
Atul Kadian <atul2143143@gmail.com>
2026-02-01 23:58:55 +0530

    fix: cors issue on prod
    Signed-off-by: Atul Kadian <atul2143143@gmail.com>

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,7 +29,7 @@ def startup():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://chat.atulkadian.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
